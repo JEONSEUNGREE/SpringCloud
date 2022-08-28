4fd1eb4e8dc42bb8b2c77215839cce5c2338d67c
전승리 <jws08219@naver.com>
2022-08-28 20:26:33 +0900

    eureka

--- springboot-cloud/eureka-exam/auction-user/src/main/java/io/namoosori/auction/user/AuctionUserApp.java
+++ springboot-cloud/eureka-exam/auction-user/src/main/java/io/namoosori/auction/user/AuctionUserApp.java
@@ -2,8 +2,10 @@ package io.namoosori.auction.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class AuctionUserApp {
     public static void main(String[] args) {
         SpringApplication.run(AuctionUserApp.class, args);
